7c2cf842424e69aa6cf10c2e22f657d283cfa456
HUYNH KIM TUYEN <huynhkimtuyenphuyen@gmail.com>
2024-11-03 23:11:37 +0700

    test thu

--- obj/Release/net8.0/RazorPagesMovie.Application.AssemblyInfo.cs
+++ obj/Release/net8.0/RazorPagesMovie.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58856d50d4a0e3d34dedab9f10024645e06bb103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90d8e2960092f605c288ab9ebeb4b46bbb7eea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
