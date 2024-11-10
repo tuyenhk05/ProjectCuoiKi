af0caf34d9a6fe8d8cd5b78e24207c83a3f03389
HUYNH KIM TUYEN <huynhkimtuyenphuyen@gmail.com>
2024-11-10 15:13:12 +0700

    1 2 3

---
+++ ClassLibrary.Repositorries/ClassLibrary.Services/Interfaces/IKoifish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.Repositorries.Entities;
+
+namespace ClassLibrary.Services.Interfaces
+{
+    public interface IKoifish
+    {
+        Task<List<IKoifish>> GetKoifishAsync();
+        Task<int> AddKoifishAsync(int koifishId);
+        Task<int> RemoveKoifishAsync(int kifishId);
+        Task<bool> DeleteKoifishAsync(int kifishId);
+        Task<int>UpdateKoifishAsync(IKoifish koifish);
+        Task<IList<KoiFish>> KoiFishs();
+    }
+}
---
+++ ClassLibrary.Repositorries/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary.Repositorries.Entities;
+
+public partial class Order
+{
+    public int OrderId { get; set; }
+
+    public int? CustomerId { get; set; }
+
+    public DateOnly? OrderDate { get; set; }
+
+    public decimal? TotalAmount { get; set; }
+
+    public string? OrderStatus { get; set; }
+
+    public virtual Customer? Customer { get; set; }
+
+    public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+}
