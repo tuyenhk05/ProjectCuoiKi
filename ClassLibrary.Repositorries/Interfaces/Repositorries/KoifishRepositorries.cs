fad94be807056dea25bb5a5c5b02dcd73d393593
HUYNH KIM TUYEN <huynhkimtuyenphuyen@gmail.com>
2024-11-10 15:16:02 +0700

    1 2 3

---
+++ ClassLibrary.Repositorries/Interfaces/Repositorries/KoifishRepositorries.cs
@@ -0,0 +1,58 @@
+﻿using ClassLibrary.Repositorries.Entities;
+using ClassLibrary.Repositorries.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Repositorries.Interfaces.Repositorries
+{
+    public class KoifishRepositorries : IKoifishRepositorries
+    {
+        private readonly QuanliwedContext _dbContext;
+        public KoifishRepositorries(QuanliwedContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int AddKoiFish(KoiFish koiFish)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public bool DelKoiFish(int koiFishId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<KoiFish>> GetKoiFish()
+        {
+            return await _dbContext.KoiFishes.ToListAsync();
+        }
+
+        public async Task<List<KoiFish>> GetKoiFishs()
+        {
+            List<KoiFish>? koifishes = null;
+            try
+            {
+                koifishes = await _dbContext.KoiFishes.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                koifishes?.Add(new KoiFish());
+            }
+
+            return koifishes;
+
+        }
+
+        public async Task<IList<KoiFish>> ToListAsync()
+        {
+            return await _dbContext.KoiFishes.ToListAsync();
+        }
+    }
+}
--- ClassLibrary.Repositorries/obj/Release/net8.0/ClassLibrary.Repositorries.AssemblyInfo.cs
+++ ClassLibrary.Repositorries/obj/Release/net8.0/ClassLibrary.Repositorries.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary.Repositorries")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0caf34d9a6fe8d8cd5b78e24207c83a3f03389")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary.Repositorries")]
