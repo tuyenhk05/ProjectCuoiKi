c1ba673c423ebcadfd6036f2b3aad44da3421900
HUYNH KIM TUYEN <huynhkimtuyenphuyen@gmail.com>
2024-11-10 14:42:40 +0700

    kkk

--- ClassLibrary.Services/obj/Debug/net8.0/ClassLibrary.Services.AssemblyInfo.cs
+++ ClassLibrary.Services/obj/Debug/net8.0/ClassLibrary.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce6f73b75b9a71b58e40135ac2d25cac4eed04f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab9a92bbe976951e323eb799f8f238480d06dfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
