e7dc49ff05a6bbf646a25dd8fdbb9e5c2b3a8da9
HUYNH KIM TUYEN <huynhkimtuyenphuyen@gmail.com>
2024-11-10 15:06:38 +0700

    update lại

---
+++ ClassLibrary.Repositorries/Entities/CartDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary.Repositorries.Entities;
+
+public partial class CartDetail
+{
+    public int CartDetailId { get; set; }
+
+    public int? CartId { get; set; }
+
+    public int? FishId { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public virtual ShoppingCart? Cart { get; set; }
+
+    public virtual KoiFish? Fish { get; set; }
+}
---
+++ ClassLibrary.Repositorries/Entities/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary.Repositorries.Entities;
+
+public partial class ShoppingCart
+{
+    public int CartId { get; set; }
+
+    public int? CustomerId { get; set; }
+
+    public virtual ICollection<CartDetail> CartDetails { get; set; } = new List<CartDetail>();
+
+    public virtual Customer? Customer { get; set; }
+}
