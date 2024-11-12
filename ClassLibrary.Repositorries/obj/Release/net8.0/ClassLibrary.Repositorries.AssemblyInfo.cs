e77235c48ae9520c820baf080a674576b327ce37
HUYNH KIM TUYEN <huynhkimtuyenphuyen@gmail.com>
2024-11-12 16:54:46 +0700

    update

--- ClassLibrary.Repositorries/obj/Release/net8.0/ClassLibrary.Repositorries.AssemblyInfo.cs
+++ ClassLibrary.Repositorries/obj/Release/net8.0/ClassLibrary.Repositorries.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary.Repositorries")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70de515e7deef837dc3f0cd4cb10fdc741935329")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary.Repositorries")]
